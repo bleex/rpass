0e3157c5a419d7a5d5311f5ca283a8854a8696e3
bleex <9681306+bleex@users.noreply.github.com>
2020-11-30 23:55:03 +0100

    Corretly convert "localhost" to C string for libssh

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use argparse::{ArgumentParser, StoreTrue, Store};
 use keepass::{Database, Node, Result};
 use std::fs;
 use std::io::Read;
+use std::process::exit;
+use libc::{c_void};
 use serde::{Serialize, Deserialize};
-use ssh::*;
+use blxlibssh::*;
+use std::ffi::CStr;
+use std::ffi::CString;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all="camelCase")]
@@ -18,64 +22,70 @@ struct Svrpass {
 
 
 fn main() -> Result<()> {
-    let mut verbose = false;
-    let mut yamlfile = "test.yml".to_string();
-    {
-        let mut ap = ArgumentParser::new();
-        ap.set_description("Change multiple passwords");
-        ap.refer(&mut verbose).add_option(&["-v", "--verbose"], StoreTrue, "Be verbose");
-        ap.refer(&mut yamlfile).add_option(&["-f", "--file", "--filename"], Store, "YAML file with options");
-        ap.parse_args_or_exit();
-    }
-    let s: String = fs::read_to_string(yamlfile)?;
-    let docs: Svrpass = serde_yaml::from_str(&s).unwrap();
-    println!("{:?}", docs);
-    // Open KeePass database
-    let path = std::path::Path::new(&docs.dbfile);
-    let db = Database::open(&mut std::fs::File::open(path)?, Some("sialababamak"), None)?;
+//    let mut verbose = false;
+//    let mut yamlfile = "test.yml".to_string();
+//    {
+//        let mut ap = ArgumentParser::new();
+//        ap.set_description("Change multiple passwords");
+//        ap.refer(&mut verbose).add_option(&["-v", "--verbose"], StoreTrue, "Be verbose");
+//        ap.refer(&mut yamlfile).add_option(&["-f", "--file", "--filename"], Store, "YAML file with options");
+//        ap.parse_args_or_exit();
+//    }
+//    let s: String = fs::read_to_string(yamlfile)?;
+//    let docs: Svrpass = serde_yaml::from_str(&s).unwrap();
+//    println!("{:?}", docs);
+//    // Open KeePass database
+//    let path = std::path::Path::new(&docs.dbfile);
+//    let db = Database::open(&mut std::fs::File::open(path)?, Some("sialababamak"), None)?;
+//
+//    let mut oldpwd_str = "";
+//    let mut newpwd_str = "";
+//    if let Some(Node::Entry(e)) = db.root.get(&[&docs.group, &docs.oldentry]) {
+//        oldpwd_str = e.get_password().unwrap();
+//    }
+//    if let Some(Node::Entry(e)) = db.root.get(&[&docs.group, &docs.newentry]) {
+//        newpwd_str = e.get_password().unwrap();
+//    }
+//    println!("{:?}", oldpwd_str);
+//    println!("{:?}", newpwd_str);
+//
+//    // Iterate over all Groups and Nodes
+//    for node in &db.root {
+//        match node {
+//            Node::Group(g) => {
+//                println!("Saw group '{0}'", g.name);
+//            },
+//            Node::Entry(e) => {
+//                let title = e.get_title().unwrap();
+//                let user = e.get_username().unwrap();
+//                let pass = e.get_password().unwrap();
+//                println!("Entry '{0}': '{1}' : '{2}'", title, user, pass);
+//            }
+//        }
+//    }
+    let mut session: ssh_session;
+    let rc: i32;
 
-    let mut oldpwd_str = "";
-    let mut newpwd_str = "";
-    if let Some(Node::Entry(e)) = db.root.get(&[&docs.group, &docs.oldentry]) {
-        oldpwd_str = e.get_password().unwrap();
-    }
-    if let Some(Node::Entry(e)) = db.root.get(&[&docs.group, &docs.newentry]) {
-        newpwd_str = e.get_password().unwrap();
-    }
-    println!("{:?}", oldpwd_str);
-    println!("{:?}", newpwd_str);
-
-    // Iterate over all Groups and Nodes
-    for node in &db.root {
-        match node {
-            Node::Group(g) => {
-                println!("Saw group '{0}'", g.name);
-            },
-            Node::Entry(e) => {
-                let title = e.get_title().unwrap();
-                let user = e.get_username().unwrap();
-                let pass = e.get_password().unwrap();
-                println!("Entry '{0}': '{1}' : '{2}'", title, user, pass);
-            }
-        }
-    }
-
-    let mut session=Session::new().unwrap();
-    session.set_host("localhost").unwrap();
-    session.set_username(&docs.user).unwrap();
-    session.parse_config(None).unwrap();
-    session.connect().unwrap();
-    session.userauth_password(oldpwd_str).unwrap();
-    {
-        let mut s=session.channel_new().unwrap();
-        s.open_session().unwrap();
-        s.request_exec(b"id").unwrap();
-        s.send_eof().unwrap();
-        let mut buf=Vec::new();
-        s.stdout().read_to_end(&mut buf).unwrap();
-        println!("{:?}",std::str::from_utf8(&buf).unwrap());
+    session = unsafe { ssh_new() };
+    if std::ptr::null() == session {
+        println!("No session");
+        exit(-1);
     }
 
+    let mut host = CString::new("localhost").unwrap();
+    unsafe { ssh_options_set(session, SSH_OPTIONS_HOST, host.as_ptr() as *const c_void) };
+    rc = unsafe { ssh_connect(session) };
+
+    if rc != SSH_OK {
+        println!("SSH Error {:?}", rc);
+        let session_ptr: *mut c_void = &mut session as *mut _ as *mut c_void; 
+        let msg_ptr =  unsafe { ssh_get_error(session_ptr) };
+        let c_str = unsafe { CStr::from_ptr(msg_ptr) };
+        println!("{:?}", c_str.to_str());
+        exit(-1);
+    }
+    unsafe { ssh_disconnect(session) };
+    unsafe { ssh_free(session) };
 
     Ok(())
 }
